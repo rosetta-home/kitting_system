97ef153d627a8c9321568e79b3cda9fec5a626ff
Christopher Coté <chris@entropealabs.com>
2017-05-10 23:06:40 -0500

    arduino builds working

--- lib/kitting_system/compiler/touchstone.ex
+++ lib/kitting_system/compiler/touchstone.ex
@@ -1,6 +1,7 @@
 defmodule KittingSystem.Compiler.Touchstone do
   require Logger
   def compile(id) do
+    test = "docker-compose run arduino-builder -compile -hardware /opt/arduino/hardware -tools /opt/arduino-builder/tools -tools /opt/arduino/hardware/tools -libraries /data/gateway -libraries /data/ieq -fqbn arduino:avr:uno -build-path /data/firmware/gateway /code/gateway/firmware.ino"
     Logger.debug "Compiling Touchstone ID: #{id}"
     [
       "priv/_compiled/touchstone/#{id}-1.hex",
