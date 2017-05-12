5c3c1e03d268eeb07f229f110fced4d34602996b
Christopher Coté <Christopher Cot>
2017-05-12 12:10:57 -0500

    make IEQ compile

--- lib/kitting_system/compiler/touchstone.ex
+++ lib/kitting_system/compiler/touchstone.ex
@@ -2,11 +2,18 @@ defmodule KittingSystem.Compiler.Touchstone do
   require Logger
   def compile(id) do
     Logger.debug "Compiling Touchstone ID: #{id}"
-    [
-      "priv/firmware/ieq/#{id}-1.ino.hex",
-      "priv/firmware/ieq/#{id}-2.ino.hex",
-      "priv/firmware/ieq/#{id}-3.ino.hex",
-      "priv/firmware/ieq/#{id}-4.ino.hex",
+    System.cmd "docker-compose", [
+      "run",
+      "arduino-builder",
+      "-compile",
+      "-hardware", "/opt/arduino/hardware",
+      "-tools", "/opt/arduino-builder/tools",
+      "-tools", "/opt/arduino/hardware/tools",
+      "-libraries", "/data/gateway",
+      "-libraries", "/data/ieq",
+      "-fqbn", "arduino:avr:uno",
+      "-build-path", "/data/firmware/ieq",
+      "/code/ieq/firmware.ino"
     ]
   end
 end
