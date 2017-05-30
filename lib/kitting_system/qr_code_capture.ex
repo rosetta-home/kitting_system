c7ad037d316c7a99ed0168f189e2b2434ea04866
Christopher Coté <Christopher Cot>
2017-05-30 14:43:27 -0500

    adds feedback for compiling and burning

--- lib/kitting_system/qr_code_capture.ex
+++ lib/kitting_system/qr_code_capture.ex
@@ -5,10 +5,12 @@ defmodule KittingSystem.QRCodeCapture do
   @collection "mac_address"
 
   def init({:tcp, :http}, req, _opts) do
-    {:ok, req, %{}}
+    {ok, req2} = :cowboy_req.chunked_reply(200, [], req)
+    Process.send_after(self(), :handle, 0)
+    {:loop, req2, %{}}
   end
 
-  def handle(req, state) do
+  def info(:handle, req, state) do
     req
     |> get
     |> verify
@@ -21,6 +23,8 @@ defmodule KittingSystem.QRCodeCapture do
     {:ok, kv, req2} = :cowboy_req.body_qs(req)
     Logger.debug "QR Code ID: #{inspect kv}"
     {_key, id} = List.keyfind(kv, "id", 0)
+    id = id |> String.downcase
+    req2 |> update_status("ID Received: #{id}")
     {req2, id}
   end
 
@@ -34,33 +38,37 @@ defmodule KittingSystem.QRCodeCapture do
       )
     id = device |> Map.get("_id") |> BSON.ObjectId.encode!
     Logger.debug "Got ID: #{id}"
+    req |> update_status("ID Verified: #{id}")
     {req, id}
   end
 
   defp compile({req, id}) do
+    req |> update_status("Compiling Touchstone: #{id}", "")
     paths = id |> Compiler.Touchstone.compile
+    req |> update_status(" - Done")
+    req |> update_status("Compiling Gateway: #{id}", "")
     paths = [id |> Compiler.Gateway.compile] ++ paths
+    req |> update_status(" - Done")
+    req |> update_status("Compiling Hub: #{id}", "")
     paths = [id |> Compiler.Hub.compile] ++ paths
+    req |> update_status(" - Done")
     {req, paths}
   end
 
   def burn({req, paths}) do
+    req |> update_status("Burning: #{paths |> Enum.join(", ")}", "")
     paths |> Harness.burn
+    req |> update_status(" - Done")
     {req, paths}
   end
 
+  def update_status(req, status, prepend \\ "\r\n") do
+    :cowboy_req.chunk("#{status}#{prepend}", req)
+  end
+
   defp reply({req, paths}, state) do
-    Logger.debug "Replying Paths: #{inspect paths}"
-    headers = [
-      {"cache-control", "no-cache"},
-      {"connection", "close"},
-      {"content-type", "text/html"},
-      {"expires", "Mon, 3 Jan 2000 12:34:56 GMT"},
-      {"pragma", "no-cache"},
-      {"Access-Control-Allow-Origin", "*"},
-    ]
-    {:ok, req2} = :cowboy_req.reply(200, headers, paths |> Enum.join(", "), req)
-    {:ok, req2, state}
+    req |> update_status("Done!")
+    {:ok, req, state}
   end
 
   defp get_filter(id) do
--- lib/kitting_system/web_server.ex
+++ lib/kitting_system/web_server.ex
@@ -10,9 +10,6 @@ defmodule KittingSystem.WebServer do
           {"/mac_address_capture", KittingSystem.MacAddressCapture, []},
           {"/qr_capture", KittingSystem.QRCodeCapture, []},
           {"/qr_reader", KittingSystem.QRCodeReader, []},
-          #{"/network", Interface.UI.Network, []},
-          #{"/connect_network", Interface.UI.ConnectNetwork, []},
-          #{"/reset_network", Interface.UI.ResetNetwork, []},
           #{"/app.js", :cowboy_static, {:priv_file, :interface, "app.js"}},
           {"/static/[...]", :cowboy_static, {:priv_dir,  :kitting_system, "ui/static"}},
 
